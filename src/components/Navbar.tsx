5f4babbf8dd80dafefb806c25d3047cb36e42555
florMignini <flormignini29@gmail.com>
2023-06-15 19:51:17 -0300

    navbar style

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import navStyles from './components.module.css'
+export const Navbar = () => {
+  return (
+    <nav className={navStyles[`nav-container`]}>
+            <h1>Hola soy Florencia</h1>
+    </nav>
+  )
+}
